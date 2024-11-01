6c3515bb2372266f8870347fe8190f97baad2036
Henrique <henriqueambiel@gmail.com>
2024-10-31 21:27:41 -0300

    Busca Binária

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -16,7 +16,8 @@ public class InventoryController : MonoBehaviour
     void Start()
     {
         itemsData = Resources.LoadAll<ItemData>("Items"); //Vai na pasta Resources, pega tudo que tem e carrega todos os itens
-        
+        BubbleSort();
+
         if(itemsData.Length > 0)
         {
             for ( int i = 0; i < itemsData.Length; i++)
@@ -35,5 +36,56 @@ public class InventoryController : MonoBehaviour
 
         TextMeshProUGUI nameText = uiItem.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         nameText.text = data.itemName;
+        TextMeshProUGUI priceText = uiItem.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
+        priceText.text = data.price.ToString("0.00");
+        TextMeshProUGUI descriptionText = uiItem.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+        descriptionText.text = data.description;
+    }
+
+    private void BubbleSort()
+    {
+        for (int i = itemsData.Length - 1; i > 0; i--)
+        {
+            bool stop = true;
+            for (int j = 0; j < i; j++)
+            {
+                if (itemsData[j].itemName.CompareTo(itemsData[j + 1].itemName) > 0)
+                {
+                    stop = false;
+                    ItemData temp = itemsData[j];
+                    itemsData[j] = itemsData[j + 1];
+                    itemsData[j + 1] = temp;
+                }
+            }
+            if (stop)
+                break;
+        }
+    }
+
+    private int BinarySearchByName(string value)
+    {
+
+        int start = 0;
+        int end = itemsData.Length - 1;
+
+        while (start <= end)
+        {
+            int middle = (start + end) / 2;
+            if (value == itemsData[middle].itemName)
+            {
+                return middle;
+            }
+
+            if (value.CompareTo(itemsData[middle].itemName) > 0)
+            {
+                start = middle + 1;
+            }
+            else if (value.CompareTo(itemsData[middle].itemName) < 0)
+            {
+                end = middle - 1;
+            }
+        }
+
+        return -1;
     }
 }
